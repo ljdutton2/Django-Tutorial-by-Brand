e86edffa1d3de383dca2c21a8197ebf56a2a3009
lauren <ljdutton2@icloud.com>
2019-11-18 13:05:30 -0800

    updates

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,11 +21,11 @@ urlpatterns = [
     path('polls/', include('polls.urls')),
     path('admin/', admin.site.urls),
     path('pizza/', include('pizza.urls')),
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
     # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    #path('<int:question_id>/results/', views.results, name='results'),
     # ex: /polls/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    #path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
